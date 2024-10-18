99c05c673dbf2ae2034d1269915c81c54896e737
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-18 18:28:36 +0200

    task 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,3 +1,3 @@
 -- a SQL script that lists all bands with Glam rock
 -- as their main style, ranked by their longevity
-SELECT band_name, (2022 - formed) AS lifespan FROM metal_bands ORDER BY lifespan DESC;
+SELECT band_name, (2022 - formed) AS lifespan FROM WHERE split IS NULL metal_bands ORDER BY lifespan DESC;
