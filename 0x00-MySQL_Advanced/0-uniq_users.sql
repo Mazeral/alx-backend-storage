14b218918ffe96618e7aa96c725dd0ee0fcbd262
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-18 17:02:52 +0200

    task 0

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,8 @@
 --  a SQL script that creates a table users following these requirements:
 -- task 0 solution:
 CREATE TABLE IF NOT EXISTS users (
-	id INT NOT NULL AUTO_INCREMENT,
-	PRIMARY KEY(id),
-	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255),
+  id INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY(id),
+  email VARCHAR(255) NOT NULL UNIQUE,
+  name VARCHAR(255)
 );
