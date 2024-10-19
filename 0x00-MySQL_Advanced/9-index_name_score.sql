10f181722260e4b8e46aeb9cb7937644744facce
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-19 18:26:29 +0200

    task 9

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -4,4 +4,4 @@
 --     Import this table dump: names.sql.zip
 --     Only the first letter of name AND score must be indexed
 
-CREATE INDEX idx_name_first ON names (name(1), names.score)
+CREATE INDEX idx_name_first ON names (name(1), score)
