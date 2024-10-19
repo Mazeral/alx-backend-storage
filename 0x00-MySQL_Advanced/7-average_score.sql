fa25fa5275d466e402e002cd47e21db15d6661fb
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-19 16:49:10 +0200

    task 7

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,16 +3,17 @@
 -- Requirements:
 --     Procedure ComputeAverageScoreForUser is taking 1 input:
 --         user_id, a users.id value (you can assume user_id is linked to an existing users)
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
 CREATE PROCEDURE ComputeAverageScoreForUser(
 	-- Defining the inputs:
 	IN user_id INT
 )
 BEGIN
-	-- Declaring variables that will contain the number of projects and the sum of the degrees
-	DECLARE projects_num DECIMAL(10,2);
-	DECLARE degrees_sum DECIMAL(10,2);
-	-- Count the number of projects:
-	SELECT COUNT(*) INTO projects_num FROM corrections WHERE corrections.user_id = user_id;
-	SELECT SUM(score) INTO degrees_sum FROM corrections WHERE corrections.user_id = user_id;
-	UPDATE users SET average_score = ( degrees_sum / projects_num ) WHERE id = user_id;
-END;
+	UPDATE users SET average_score = ( SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id )
+	WHERE id = user_id;
+END $$
+
+DELIMITER ;
