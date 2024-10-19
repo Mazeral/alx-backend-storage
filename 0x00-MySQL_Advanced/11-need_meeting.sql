57dec538968a250f18e0ba2f0cbaf56184dfdf22
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-19 20:11:29 +0200

    task 11 fix 2

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -7,4 +7,4 @@
 DROP VIEW IF EXISTS need_meeting;
 
 CREATE VIEW need_meeting AS
-SELECT name FROM students WHERE score < 80 AND last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
+SELECT name FROM students WHERE score < 80 AND (last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH) OR last_meeting IS NULL);
