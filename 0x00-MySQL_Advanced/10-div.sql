5f482d658f894363989eea41077eecb77532159d
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-19 18:45:25 +0200

    task 10 fix 2

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -6,6 +6,8 @@
 --         a, INT
 --         b, INT
 --     And returns a / b or 0 if b == 0
+DELIMITER //
+
 CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT
 DETERMINISTIC
@@ -13,4 +15,6 @@ BEGIN
 	IF b = 0 THEN RETURN 0;
 	END IF;
 	RETURN a / b;
-END;
+END //
+
+DELIMITER;
