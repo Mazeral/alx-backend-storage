12791d5b75b12dc5173a6de5ed29ff989d469ceb
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-22 16:30:17 +0200

    fixing task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -6,30 +6,30 @@ Module for interacting with a MongoDB database.
 This module provides a function to list all documents in a MongoDB collection.
 """
 
-if __name__ == "__main__":
-    def list_all(mongo_collection):
-        """
-        Retrieves all documents from a MongoDB collection.
 
-        Args:
-            mongo_collection (str): The name of the MongoDB collection.
+def list_all(mongo_collection):
+    """
+    Retrieves all documents from a MongoDB collection.
 
-        Returns:
-            list: A list of documents from the MongoDB collection.
-            If the collection is empty, an empty list is returned.
+    Args:
+        mongo_collection (str): The name of the MongoDB collection.
 
-        Raises:
-            pymongo.errors.PyMongoError: If an error occurs while connecting
-            to the MongoDB database or retrieving documents.
-        """
+    Returns:
+        list: A list of documents from the MongoDB collection.
+        If the collection is empty, an empty list is returned.
 
-        # Retrieve all documents from the collection
-        documents = mongo_collection.find()
+    Raises:
+        pymongo.errors.PyMongoError: If an error occurs while connecting
+        to the MongoDB database or retrieving documents.
+    """
 
-        # Check if the collection is empty
-        if documents.count() == 0:
-            # Return an empty list if the collection is empty
-            return []
-        else:
-            # Return the list of documents
-            return documents
+    # Retrieve all documents from the collection
+    documents = mongo_collection.find()
+
+    # Check if the collection is empty
+    if documents.count() == 0:
+        # Return an empty list if the collection is empty
+        return []
+    else:
+        # Return the list of documents
+        return documents
