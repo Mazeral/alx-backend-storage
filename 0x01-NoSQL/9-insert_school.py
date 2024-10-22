1a6aafda251ce52ca7fddfd1a1dd6496030152d8
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-22 16:32:56 +0200

    fixing and docuementing task 9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python3
 
-if __name__ == "__main__":
-    def insert_school(mongo_collection, **kwargs):
-        return mongo_collection.insert(**kwargs)
+def insert_school(mongo_collection, **kwargs):
+    """
+    Inserts a new document into the specified MongoDB collection.
+
+    Args:
+        mongo_collection: A MongoDB collection object where the document
+        will be inserted.
+        **kwargs: Keyword arguments representing the document to be inserted.
+            Each key-value pair will be used as a field in the document.
+
+    Returns:
+        The result of the insert operation, as per the MongoDB Python driver's
+        API.
+            Typically, this includes the `_id` of the inserted document and an
+            `acknowledged` status.
+
+    Raises:
+        Exception: Any exception raised by the MongoDB Python driver during the
+        insert operation.
+    """
+    return mongo_collection.insert(**kwargs)
