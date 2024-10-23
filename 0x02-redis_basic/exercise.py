db21365a6cfbb5a3599dc0e876bc9ab1754805d4
MohammadOSA <mohamedsiddig5026485@gmail.com>
2024-10-23 13:52:16 +0200

    task 3 fix 2

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -23,6 +23,44 @@ from typing import Union, Callable, Optional
 from functools import wraps
 
 
+def count_calls(method: Callable) -> Callable:
+    """
+    A decorator that increments a Redis counter each time the decorated
+    method is called.
+
+    Args:
+        method (Callable): The method to be decorated.
+
+    Returns:
+        Callable: The decorated method.
+    """
+    Protip: when defining a decorator it is useful to
+    use functool.wraps to conserve the original function’s name, docstring, etc.
+    @wraps(method)
+    # By including self as the first parameter in the wrapper function,
+    # you ensure that the wrapper has access to the same instance attributes
+    # and methods as the original method
+    def wrapper(self, *args, **kwargs):
+        """
+        The wrapper function that increments the Redis counter and
+        calls the original method.
+
+        Args:
+            self: The instance of the class that the method belongs to.
+
+        Returns:
+            Any: The result of the original method call.
+        """
+        # Get the qualified name of the method to use as the Redis key.
+        qualname = method.__qualname__
+
+        # Increment the Redis counter for the method.
+        self._redis.incr(qualname)
+
+        # Call the original method and return its result.
+        return method(self, *args, **kwargs)
+
+    return wrapper
 
 
 class Cache:
@@ -47,17 +85,6 @@ class Cache:
         """
         self._redis = redis.Redis()
         self._redis.flushdb()
-
-    @staticmethod
-    def count_calls(method: Callable) -> Callable:
-        @wraps(method)
-        def wrapper(self, *args, **kwargs):
-            qualname = method.__qualname__
-            self._redis.incr(qualname)
-            return method(self, *args, **kwargs)
-        return wrapper
-
-    return wrapper
 
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
